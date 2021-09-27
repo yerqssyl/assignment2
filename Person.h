8572f3adca816883bfe6acaf565dfd17a07c592c
yerqssyl <80276457+yerqssyl@users.noreply.github.com>
2021-09-27 23:17:37 +0600

    Add files via upload

---
+++ Person.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Person {
+private:
+    string name;
+    int age;
+    int luck;
+public:
+    Person();
+    Person(const string &name, int age, int luck);
+
+    const string &getName() const;
+
+    void setName(const string &name);
+
+    int getAge() const;
+
+    void setAge(int age);
+
+    int getLuck() const;
+
+    void setLuck(int luck);
+};
+
+
+#endif //ASSIGNMENTTWO_PERSON_H
